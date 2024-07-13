b3dc11438fab0167acc189baa81bca8dc1029e98
Abraham John <jabrahamjohns@gmail.com>
2024-07-13 22:36:27 +0300

    fix:stats file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,7 +11,8 @@ os.chmod('0-stats.py', 0o755)
 if __name__ == '__main__':
     """ Main function """
     file_size = [0]
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
     def print_stats():
         """ Print statistics """
